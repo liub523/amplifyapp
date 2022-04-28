3a11f093dcff5b9c907ae4e157496eadbfee825f
liub523 <443008107@qq.com>
2022-04-28 09:30:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
+        <h1>Hello world!</h1>
       </header>
     </div>
   );
